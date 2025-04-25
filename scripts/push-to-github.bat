1ac799a0102ba4cfa34bdbb3411d905d168169ad
Radhesham Waghchaure <radheshamw@prescient.in>
2025-04-25 16:15:10 +0530

    update the build

--- scripts/push-to-github.bat
+++ scripts/push-to-github.bat
@@ -1,7 +1,6 @@
 @echo off
 echo ==== Pushing Build Zip to GitHub ====
-cd ..\dist
-git add *.zip
-git commit -m "Add build zip"
-git push --set-upstream origin main
-
+set GIT="C:\Program Files\Git\cmd\git.exe"
+%GIT% add build_*.zip
+%GIT% commit -m "Add zip"
+%GIT% push origin main
