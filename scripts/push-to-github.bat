3c35819f83ba43cb303c5d7f933ed77245086334
Radhesham Waghchaure <radheshamw@prescient.in>
2025-04-25 16:12:29 +0530

    update the build

--- scripts/push-to-github.bat
+++ scripts/push-to-github.bat
@@ -1,17 +1,4 @@
-@echo off
-echo ==== Pushing Build Zip to GitHub ====
-cd dist
 
-REM Configure Git identity
-git config user.name "jenkins"
-git config user.email "jenkins@example.com"
-
-REM Initialize repo if needed
-if not exist .git (
-    git init
-    git remote add origin https://github.com/YourUsername/Calculator.git
-)
-
-git add *.zip
-git commit -m "Automated build zip"
-git push https://%GIT_USER%:%GIT_PASS%@github.com/YourUsername/Calculator.git main
+"C:\Users\radheshamw\AppData\Local\Programs\Git\cmd\git.exe" add build_*.zip
+"C:\Users\radheshamw\AppData\Local\Programs\Git\cmd\git.exe" commit -m "Add zip"
+"C:\Users\radheshamw\AppData\Local\Programs\Git\cmd\git.exe" push
