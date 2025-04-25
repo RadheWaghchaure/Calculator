0074a59ed535304b0f0de952b79c1e2d034dad96
Radhesham Waghchaure <radheshamw@prescient.in>
2025-04-25 15:53:33 +0530

    update the build

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,19 +1,19 @@
 @echo off
 echo ===== Running JUnit Tests =====
 
-set LIB=..\lib
+set LIB=lib
 set CLASSPATH=.;%LIB%\junit-4.13.2.jar;%LIB%\hamcrest-core-1.3.jar
 
-if not exist ..\test-classes mkdir ..\test-classes
+if not exist test-classes mkdir test-classes
 
-javac -cp %CLASSPATH% -d ..\test-classes ..\Calculator.java ..\CalculatorTest.java
+javac -cp %CLASSPATH% -d test-classes src\Calculator.java test\CalculatorTest.java
 
 if %errorlevel% neq 0 (
     echo Compilation failed.
     exit /b %errorlevel%
 )
 
-java -cp %CLASSPATH%;..\test-classes org.junit.runner.JUnitCore CalculatorTest
+java -cp %CLASSPATH%;test-classes org.junit.runner.JUnitCore CalculatorTest
 
 echo Test execution complete.
 
