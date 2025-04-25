ec91e4caf4ed5aef7e4ad0e2294e1546c37c9028
Radhesham Waghchaure <radheshamw@prescient.in>
2025-04-25 15:31:29 +0530

    update the build

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,13 @@
 @echo off
 echo ===== Compiling Java Source =====
-mkdir ..\build
+
+rem Create the build directory if it doesn't exist
+if not exist ..\build (
+    mkdir ..\build
+)
+
+rem Compile Calculator.java and output .class to build folder
 javac -d ..\build ..\Calculator.java
-echo Compilation completed.
+
+echo ✅ Compilation completed.
+
